83d47eb9123544253855a6e9ae6d3302926dbffe
Gautam kumar jaiswal <42580621+thegautamkumarjaiswal@users.noreply.github.com>
2021-05-04 10:58:46 +0530

    files

---
+++ main.py
@@ -0,0 +1,12 @@
+### Imports ###
+# add imports - classes and defs
+import sys
+from predictor import predictRuns
+
+
+"""
+sys.argv[1] is the input test file name given as command line arguments
+
+"""
+runs = predictRuns('inputFile.csv')
+print("Predicted Runs: ", runs)
---
+++ predictor.py
@@ -0,0 +1,6 @@
+### Custom definitions and classes if any ###
+
+def predictRuns(testInput):
+    prediction = 0
+    ### Your Code Here ###
+    return prediction
