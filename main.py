500088ba9b83c3908ac5b760b39b162692afd3d0
Gautam kumar jaiswal <42580621+thegautamkumarjaiswal@users.noreply.github.com>
2021-06-05 12:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 ### Imports ###
 # add imports - classes and defs
-import sys
 from predictor import predictRuns
 
 
@@ -10,4 +9,3 @@ sys.argv[1] is the input test file name given as command line arguments
 """
 runs = predictRuns('inputFile.csv')
 print("Predicted Runs: ", runs)
-
